3dbeeb564509424796107f4ab59c674bee3fd094
Maciej Kotowski <makoteq@gmail.com>
2022-10-11 20:57:52 +0200

    fix formating and version

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ let args = process.argv.slice(2);
 
 const usage = `
 ${chalk.yellowBright.bold("Command: ")}
-  ${chalk.green("start")} ${chalk.cyan("<file_path> [<delay>]")} : "Show text from file in <file_path> with delay between words in <delay>" ${chalk.cyan("(default 500ms)")}
+  ${chalk.green("start")} ${chalk.cyan(
+  "<file_path> [<delay>]"
+)} : "Show text from file in <file_path> with delay between words in <delay>" ${chalk.cyan(
+  "(default 500ms)"
+)}
 
 ${chalk.yellowBright.bold("Argument Details: ")}
   ${chalk.green("file_path")} : full or absolute path of file [mandatory*]
@@ -17,11 +21,14 @@ ${chalk.yellowBright.bold("Argument Details: ")}
 ${chalk.yellowBright.bold("Options: ")}
   ${chalk.green("-h, --help")} : show help
   ${chalk.green("-v, --version")} : show version
-`
+`;
 
 switch (args[0]) {
   default:
-    show("METAXA", "Run - metaxa start <path> to begin reading\nIf you want to know more, explore help section by\nmetaxa -h or metaxa --help");
+    show(
+      "METAXA",
+      "Run - metaxa start <path> to begin reading\nIf you want to know more, explore help section by\nmetaxa -h or metaxa --help"
+    );
 
     break;
   case "-help":
@@ -30,16 +37,18 @@ switch (args[0]) {
     break;
   case "-version":
   case "-v":
-    let rawdata = fs.readFileSync('./package.json');
+    let rawdata = fs.readFileSync("./package.json");
     let pjson = JSON.parse(rawdata);
     console.log(pjson.version);
-    break
+    break;
   case "start":
     if (args[1] && path.extname(args[1]) == ".txt") {
       fs.readFile(args[1], "utf8", function (err, data) {
         if (err) {
-          console.log(chalk.white.bgRed.bold("\nSomething went wrong..."))
-          console.log(`\n${chalk.yellowBright.bold('Error')}: ${chalk.red(err.message)}`);
+          console.log(chalk.white.bgRed.bold("\nSomething went wrong..."));
+          console.log(
+            `\n${chalk.yellowBright.bold("Error")}: ${chalk.red(err.message)}`
+          );
           return;
         }
         projector(
@@ -49,7 +58,12 @@ switch (args[0]) {
             : 500
         );
       });
-    } else console.log(`\n${chalk.yellowBright.bold('Error')}: ${chalk.red("You need to specify proper path")}`);
+    } else
+      console.log(
+        `\n${chalk.yellowBright.bold("Error")}: ${chalk.red(
+          "You need to specify proper path"
+        )}`
+      );
 
     break;
 }
